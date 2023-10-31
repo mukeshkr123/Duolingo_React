806ea58627d35da6d1424feb33f5d1169c29246b
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-31 17:21:07 +0530

    add props and do mapping

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { Image, Text, View } from "react-native";
 import styles from "./App.styles";
 import ImageOption from "./src/components/ImageOption/index";
+import question from "./assets/data/oneQuestionWithOption";
 
 export default function App() {
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>Which of these is the "glass"?</Text>
+      <Text style={styles.title}>{question.question}</Text>
+
       <View style={styles.optionsContainer}>
-        <ImageOption />
+        {question.options.map((option) => (
+          <ImageOption
+            key={option.id}
+            image={option.image}
+            text={option.text}
+          />
+        ))}
       </View>
     </View>
   );
--- assets/data/oneQuestionWithOption.js
+++ assets/data/oneQuestionWithOption.js
@@ -5,25 +5,26 @@ export default {
       id: "option1",
       image:
         "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/cup.png",
+      text: "Cup",
     },
     {
       id: "option2",
       image:
         "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png",
-      text: "el vaso",
+      text: "Glass",
       correct: true,
     },
     {
       id: "option3",
       image:
         "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/milk.png",
-      text: "la leche",
+      text: "Milk",
     },
     {
       id: "option4",
       image:
         "https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/coffee.png",
-      text: "el café",
+      text: "Coffee",
     },
   ],
 };
