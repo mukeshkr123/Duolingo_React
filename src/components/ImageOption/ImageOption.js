b61c153d5cb30bcb94d0784e0431a6ef4b2d8ddd
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-31 17:31:12 +0530

    add propTypes for ImageOption

--- src/components/ImageOption/ImageOption.js
+++ src/components/ImageOption/ImageOption.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import styles from "./styles";
+import PropTypes from "prop-types";
 
 const ImageOption = ({ image, text }) => {
   return (
@@ -12,9 +13,14 @@ const ImageOption = ({ image, text }) => {
         resizeMode="contain"
         style={styles.optionImage}
       />
-      <Text style={styles.optionText}>{text}</Text>
+      <Text style={styles.optionText}>{text.toUpperCase()}</Text>
     </View>
   );
+};
+
+ImageOption.propTypes = {
+  image: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default ImageOption;
